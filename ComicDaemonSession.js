4bb9b8158dc290977d7a16f5df9288397fcb3663
wandyezj <wandyezj@gmail.com>
2021-04-21 19:56:46 -0700

    change to spaces and recommend browsers

--- ComicDaemonSession.js
+++ ComicDaemonSession.js
@@ -1,53 +1,53 @@
 var Session = undefined;
 
 function ClearSession(){
-	if (localStorage != undefined) {
-		localStorage.clear();
-	}
+    if (localStorage != undefined) {
+        localStorage.clear();
+    }
 }
 
 function LoadSession() {
-	// Code for localStorage/sessionStorage.
-	
-	//alert(typeof(Storage) !== "undefined");
-	//alert(localStorage != "undefined");
-	//alert(localStorage["Session"]);
+    // Code for localStorage/sessionStorage.
+    
+    //alert(typeof(Storage) !== "undefined");
+    //alert(localStorage != "undefined");
+    //alert(localStorage["Session"]);
 
-	//define empty session object
-	Session = {};
-	
-	if (typeof(Storage) !== "undefined") {
-		if (localStorage != undefined && localStorage["Session"] != undefined) {
-			var data = localStorage["Session"];
-			//alert(data);
-			Session = JSON.parse(data);
-		}
-	}
+    //define empty session object
+    Session = {};
+    
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage != undefined && localStorage["Session"] != undefined) {
+            var data = localStorage["Session"];
+            //alert(data);
+            Session = JSON.parse(data);
+        }
+    }
 }
-		
+        
 function SaveSession() {
-	if (typeof(Storage) !== "undefined") {
-		if (localStorage != undefined) {
-			var data = JSON.stringify(Session);
-			//alert(data);
-			localStorage.setItem("Session", data);
-		}
-	}
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage != undefined) {
+            var data = JSON.stringify(Session);
+            //alert(data);
+            localStorage.setItem("Session", data);
+        }
+    }
 }
 
 function SessionGetLastVisitTime() {
-	var lastVisitTime = undefined;
-	
-	if (Session != undefined && Session["LastVisitTime"] != undefined) {
-		lastVisitTime = Session["LastVisitTime"];
-	}
-	
-	return lastVisitTime;
+    var lastVisitTime = undefined;
+    
+    if (Session != undefined && Session["LastVisitTime"] != undefined) {
+        lastVisitTime = Session["LastVisitTime"];
+    }
+    
+    return lastVisitTime;
 }
 
 function SessionSetLastVisitTime() {
-	Session["LastVisitTime"] = Date.now();
-	SaveSession();
+    Session["LastVisitTime"] = Date.now();
+    SaveSession();
 }
 
 var SESSION_COMICS = "comics";
@@ -57,67 +57,67 @@ var STAR_TAG = "star";
 var DEFAULT_HIDE_LIST = [];
 
 function SessionSetLastComicNumber(comicName, comicNumber) {
-	if (Session == undefined) {
-		// Programming Error
-		alert("SessionSetLastComicNumber Session undefined");
-		return;
-	}
+    if (Session == undefined) {
+        // Programming Error
+        alert("SessionSetLastComicNumber Session undefined");
+        return;
+    }
 
-	if (undefined == Session[SESSION_COMICS]) {
-		Session["comics"] = {};
-	}
-	
-	var comics = Session[SESSION_COMICS];
-	
-	if (undefined == comics[comicName]){
-		comics[comicName] = {};
-	}
-	
-	comics[comicName][SESSION_COMIC_NUMBER] = comicNumber;
-	SaveSession();
+    if (undefined == Session[SESSION_COMICS]) {
+        Session["comics"] = {};
+    }
+    
+    var comics = Session[SESSION_COMICS];
+    
+    if (undefined == comics[comicName]){
+        comics[comicName] = {};
+    }
+    
+    comics[comicName][SESSION_COMIC_NUMBER] = comicNumber;
+    SaveSession();
 }
 
 function SessionGetLastComicNumber(comicName) {
-	if (undefined == Session) {
-		// Programming Error
-		alert("SessionGetLastComicNumber Session undefined");
-		return 0;
-	}
+    if (undefined == Session) {
+        // Programming Error
+        alert("SessionGetLastComicNumber Session undefined");
+        return 0;
+    }
 
-	if (undefined == Session[SESSION_COMICS] 
-		|| undefined == Session[SESSION_COMICS][comicName]
-		|| undefined == Session[SESSION_COMICS][comicName][SESSION_COMIC_NUMBER]
-		){
-		
-		return 0;
-	}
+    if (undefined == Session[SESSION_COMICS] 
+        || undefined == Session[SESSION_COMICS][comicName]
+        || undefined == Session[SESSION_COMICS][comicName][SESSION_COMIC_NUMBER]
+        ){
+        
+        return 0;
+    }
 
-	return Session[SESSION_COMICS][comicName][SESSION_COMIC_NUMBER];
+    return Session[SESSION_COMICS][comicName][SESSION_COMIC_NUMBER];
 }
 
 
 function SessionGetHideTagList() {
-	if (undefined == Session) {
-		// Programming Error
-		alert("SessionGetHideList Session undefined");
-		return DEFAULT_HIDE_LIST;
-	}
+    if (undefined == Session) {
+        // Programming Error
+        alert("SessionGetHideList Session undefined");
+        return DEFAULT_HIDE_LIST;
+    }
 
-	if (undefined == Session[SESSION_HIDE_TAGS_LIST]) {
-		return DEFAULT_HIDE_LIST;
-	}
-	
-	return Session[SESSION_HIDE_TAGS_LIST];
-	
+    if (undefined == Session[SESSION_HIDE_TAGS_LIST]) {
+        return DEFAULT_HIDE_LIST;
+    }
+    
+    return Session[SESSION_HIDE_TAGS_LIST];
+    
 }
 
 function SessionSetHideTagList(hideList) {
-	if (Session == undefined) {
-		// Programming Error
-		alert("SessionSetHideList Session undefined");
-		return;
-	}
+    if (Session == undefined) {
+        // Programming Error
+        alert("SessionSetHideList Session undefined");
+        return;
+    }
 
-	Session[SESSION_HIDE_TAGS_LIST] = hideList;
-	SaveSession();
+    Session[SESSION_HIDE_TAGS_LIST] = hideList;
+    SaveSession();
 }
--- helpers.js
+++ helpers.js
@@ -1,62 +1,62 @@
 // Does list contain any of the elements in the any list?
 function ListContainsAny(list, anyList) {
-	let i;
-	for (let i = 0; i < anyList.length; i++){
-		let currentAny = anyList[i];
-		if (undefined != list.find(function(element){return element == currentAny;})) {
-			return true;
-		}
-	}
-	return false;
+    let i;
+    for (let i = 0; i < anyList.length; i++){
+        let currentAny = anyList[i];
+        if (undefined != list.find(function(element){return element == currentAny;})) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function ListFindValueIndex(list, value) {
-	let index = list.findIndex(function(element){return element == value;});
-	
-	if (-1 == index) {
-		index = undefined;
-	}
-	
-	return index;
+    let index = list.findIndex(function(element){return element == value;});
+    
+    if (-1 == index) {
+        index = undefined;
+    }
+    
+    return index;
 }
 
 function ListContainsValue(list, value) {
-	return (undefined != ListFindValueIndex(list, value));
+    return (undefined != ListFindValueIndex(list, value));
 }
 
 function ListRemoveValue(list, value) {
-	let index = ListFindValueIndex(list, value);
-	while (undefined != index) {
-		list.splice(index, 1);
-		index = ListFindValueIndex(list, value);
-	} 
+    let index = ListFindValueIndex(list, value);
+    while (undefined != index) {
+        list.splice(index, 1);
+        index = ListFindValueIndex(list, value);
+    } 
 }
 
 // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Synchronous_and_Asynchronous_Requests
 
 //returns a json object of the files contents from sever
 function LoadFileJson(filepath) {
-	let xmlhttp = new XMLHttpRequest();
-	
-	// Need to make this async (set to true and then spin wait...)
-	xmlhttp.open("GET", filepath, false);
-	xmlhttp.send();
-	
-	return JSON.parse(xmlhttp.responseText);
+    let xmlhttp = new XMLHttpRequest();
+    
+    // Need to make this async (set to true and then spin wait...)
+    xmlhttp.open("GET", filepath, false);
+    xmlhttp.send();
+    
+    return JSON.parse(xmlhttp.responseText);
 }
 
 function CacheFile(filepath) {
-	let xmlhttp = new XMLHttpRequest();
-	
-	// Async Request to Cache.
-	xmlhttp.open("GET", filepath, true);
-	xmlhttp.send();
+    let xmlhttp = new XMLHttpRequest();
+
+    // Async Request to Cache.
+    xmlhttp.open("GET", filepath, true);
+    xmlhttp.send();
 }
 
 function Show(id) {
-	document.getElementById(id).style.display =  'block';
+    document.getElementById(id).style.display =  'block';
 }
 
 function Hide(id) {
-	document.getElementById(id).style.display =  'none';
+    document.getElementById(id).style.display =  'none';
 }
